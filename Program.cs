1e0676bf390890503ff8f276791aaed996c8226e
BOMBUSVS <vsevoloddesign8@gmail.com>
2022-08-12 19:46:44 +0300

    Console-Program-Payroll-for-employees
    Етап проектування програми LLCpayrollSoftwareDevelop та Інструкція по роботі з програмою LLCpayrollSoftwareDevelop - додається в описі в форматі OpenDocument (.odt) LLC payroll Software program MANUAL and Design stage of the LLC payroll Software Develop program attached in the description in OpenDocument format (.odt)

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Vsevolod.LLCpayrollSoftware.Domain;
+using Vsevolod.LLCpayrollSoftware.Persistence;
+
+namespace LLCpayrollSoftwareDevelop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager("test", new List<TimeRecord>()
+            {
+                new TimeRecord(DateTime.Now.AddDays(-3),"test",8,"test message"),
+                new TimeRecord(DateTime.Now.AddDays(-2),"test",9,"test message"),
+                new TimeRecord(DateTime.Now.AddDays(-1),"test",7,"test message"),
+            });
+
+            Console.WriteLine(manager.TotalPay); 
+            Console.ReadLine();
+        }
+    }
+}
